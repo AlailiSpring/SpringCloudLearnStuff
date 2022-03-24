dac6245195faf6950acc2d3ef59e23925f1101d4
AlailiSpring <libdqd@126.com>
2022-03-24 23:31:56 +0800

    1、加入新的子项目cloud-api-commons,作为公有类使用 2、去掉其余两个子项目中冗余的公用类

--- cloud-consumer-order80/src/main/java/com/alaili/springcloud/controller/Order80Controller.java
+++ cloud-consumer-order80/src/main/java/com/alaili/springcloud/controller/Order80Controller.java
@@ -1,8 +1,12 @@
 package com.alaili.springcloud.controller;
 
+import com.alaili.springcloud.entities.CommonResult;
+import com.alaili.springcloud.entities.Payment;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
 
 @RestController
 public class Order80Controller {
@@ -10,7 +14,16 @@ public class Order80Controller {
     @Value("${remoteinfo.remoteUrl}")
     private String remoteUrl;
 
+    @Resource
     private RestTemplate restTemplate;
 
+    @PostMapping("/consume/payment/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(remoteUrl + "/payment/create", payment, CommonResult.class);
+    }
 
+    @GetMapping("/consume/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(remoteUrl + "/payment/get/" + id, CommonResult.class);
+    }
 }
