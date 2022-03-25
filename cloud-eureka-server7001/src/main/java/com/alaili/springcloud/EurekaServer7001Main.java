fcb17518fae4f4385bd93ba1b97bf66d5956f145
AlailiSpring <libdqd_happy2020@163.com>
2022-03-25 16:40:59 +0800

    1、加入eureka子项目

---
+++ cloud-eureka-server7001/src/main/java/com/alaili/springcloud/EurekaServer7001Main.java
@@ -0,0 +1,12 @@
+package com.alaili.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer7001Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001Main.class, args);
+    }
+}
