d297e74b4fd42d0aa17fed56921b1e824a5f2845
AlailiSpring <libdqd@126.com>
2022-03-28 23:57:06 +0800

    1、加入Hystrix服务熔断降级处理机制

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/alaili/springcloud/HystrixPayment8001Main.java
@@ -0,0 +1,20 @@
+package com.alaili.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @Description: 熔断代码主程序
+ * @Author: LiBaoDeng
+ * @Date: 2022-03-28 23:40
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableCircuitBreaker//启动hystrix服务降级
+public class HystrixPayment8001Main {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixPayment8001Main.class, args);
+    }
+}
---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/alaili/springcloud/controller/PaymentController.java
@@ -0,0 +1,65 @@
+package com.alaili.springcloud.controller;
+
+import com.alaili.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Description:
+ * @Author: LiBaoDeng
+ * @Date: 2022-03-28 23:47
+ */
+@RestController
+public class PaymentController {
+
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(PaymentController.class);
+
+    @Resource
+    private PaymentService service;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    /**
+     * 正常访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id) {
+        String result = service.paymentInfo_OK(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id) {
+        String result = service.paymentInfo_TimeOut(id);
+        log.info("*****result:" + result);
+        return result;
+    }
+
+    /**
+     * 服务熔断
+     * http://localhost:8001/payment/circuit/32
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/circuit/{id}")
+    public String paymentCircuitBreaker(@PathVariable("id") Integer id) {
+        String result = service.paymentCircuitBreaker(id);
+        log.info("***result:" + result);
+        return result;
+    }
+}
