6fc5cfcc47442a16c1d90bbcfcae2b0eaa97b1c7
AlailiSpring <libdqd_happy2020@163.com>
2022-03-30 13:42:11 +0800

    1、提交hystrix监控相关的代码

--- cloud-provider-hystrix-payment8001/src/main/java/com/alaili/springcloud/HystrixPaymentMain8001.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/alaili/springcloud/HystrixPaymentMain8001.java
@@ -1,9 +1,12 @@
 package com.alaili.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Description: 熔断代码主程序
@@ -16,5 +19,18 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class HystrixPaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(HystrixPaymentMain8001.class, args);
+    }
+    /**
+     * 添加Hystrix检测
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
